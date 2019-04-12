4e9ed17cb5dbdac16db5dd825a3733e6dee058c0
Dmitry Fonarev <dfonarev@e1z2r4p9.42.us.org>
2019-04-11 21:41:44 -0700

    edit

--- queue.c
+++ queue.c
@@ -1,5 +1,5 @@
 #include <stdlib.h>
-#include <stdio.h>
+#include <stdio.h> // KILLME
 
 struct s_node {
 	void *content;
@@ -13,7 +13,7 @@ struct s_queue {
 
 struct s_queue	*init(void)
 {
-	struct s_queue	*q;
+	struct s_queue *q;
 
 	if (!(q = malloc(sizeof(*q))))
 		return (NULL);
@@ -22,56 +22,61 @@ struct s_queue	*init(void)
 	return (q);
 }
 
-void			enqueue(struct s_queue *queue, void *content)
+void	enqueue(struct s_queue *queue, void *content)
 {
-	struct s_node	*new;
+	struct s_node        *new;
 
-	if (!queue)
-		return ;
-	if (!(new = malloc(sizeof(*new))))
-		return ;
-	new->content = content;
-	new->next = NULL;
-	if (!queue->first)
-		queue->first = new;
-	else
-		queue->last->next = new;
-	queue->last = new;
+	if (queue)
+	{
+		if (!(new = malloc(sizeof(*new))))
+			return ;
+		new->content = content;
+		new->next = NULL;
+		if (!queue->first)
+			queue->first = new;
+		else
+			queue->last->next = new;
+		queue->last = new;
+	}
 }
 
-void			*dequeue(struct s_queue *queue)
+void	*dequeue(struct s_queue *queue)
 {
-	struct s_node	*tmp;
-	void			*content;
+	struct s_node        *tmp;
+	void                        *ret;
 
-	if (!queue)
-		return (NULL);
-	content = queue->first->content;
-	tmp = queue->first;
-	queue->first = queue->first->next;
-	free(tmp);
-	return (content);
+	ret = NULL;
+	if (queue && queue->first)
+	{
+		if (queue->first == queue->last)
+			queue->last = NULL;
+		tmp = queue->first;
+		ret = tmp->content;
+		queue->first = tmp->next;
+		free(tmp);
+	}
+	return (ret);
 }
 
-void			*peek(struct s_queue *queue)
+void	*peek(struct s_queue *queue)
 {
-	if (!(queue))
+	if (!queue || !queue->first)
 		return (NULL);
 	return (queue->first->content);
 }
 
-int				isEmpty(struct s_queue *queue)
+int		isEmpty(struct s_queue *queue)
 {
-	if (!(queue->first))
+	if (!queue || !queue->first)
 		return (1);
 	return (0);
 }
 
-int				main()
+int		main()
 {
-	char	s1[] = "fuck\n";
-	char	s2[] = "off\n";
-	struct s_queue	*q;
+	char        s1[] = "fuck\n";
+	char        s2[] = "off\n";
+	struct s_queue        *q;
 
 	q = init();
 	printf("empty yes %i\n", isEmpty(q));
@@ -83,5 +88,4 @@ int				main()
 	printf("empty no %i\n", isEmpty(q));
 	printf("peek s2 %s", peek(q));
 	printf("deq s2 %s", dequeue(q));
-	printf("empty yes %i\n", isEmpty(q));
 }
