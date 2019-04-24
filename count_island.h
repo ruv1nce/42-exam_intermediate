841692012f0f44d1ecb1e2c666765d89b1e95601
Dmitry Fonarev <dfonarev@e1z2r4p9.42.us.org>
2019-04-23 23:30:44 -0700

    island

---
+++ count_island.h
@@ -0,0 +1,13 @@
+#ifndef COUNT_ISLAND_H
+# define COUNT_ISLAND_H
+
+# include <fcntl.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <stdio.h>
+
+# define BUF_SIZE 20
+
+#endif
