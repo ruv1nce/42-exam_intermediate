e151ebb7b7f16cf3d3db16eb48bc6d205bedf0e7
Dmitry Fonarev <dfonarev@e1z2r4p9.42.us.org>
2019-05-08 03:18:59 -0700

    edit

---
+++ README.md
@@ -0,0 +1,6 @@
+### 42-exam_intermediate
+
+TODO:</br>
+str_maxlenoc: easier-to-read version</br>
+clone_list O(n)</br>
+g_diam
---
+++ flood_fill.c
@@ -0,0 +1,21 @@
+typedef struct	s_point
+{
+	int			x;
+	int			y;
+}				t_point;
+
+void flood_fill(char **tab, t_point size, t_point begin)
+{
+	char	c;
+
+	c = tab[begin.y][begin.x];
+	tab[begin.y][begin.x] = 'F';
+	if ((begin.x + 1 < size.x) && (tab[begin.y][begin.x + 1] == c))
+		flood_fill(tab, size, (t_point){begin.x + 1, begin.y});
+	if (begin.x > 0 && (tab[begin.y][begin.x - 1] == c))
+		flood_fill(tab, size, (t_point){begin.x - 1, begin.y});
+	if ((begin.y + 1 < size.y) && (tab[begin.y + 1][begin.x] == c))
+		flood_fill(tab, size, (t_point){begin.x, begin.y + 1});
+	if (begin.y > 0 && (tab[begin.y - 1][begin.x] == c))
+		flood_fill(tab, size, (t_point){begin.x, begin.y - 1});
+}
